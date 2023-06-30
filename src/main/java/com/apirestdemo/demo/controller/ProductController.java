e9821ea961f9aed9f2f029a6b352292e737c1db1
jeremiasfreitass <jeremiasjmf@hotmail.com>
2023-06-30 15:02:06 -0300

    implementação realizada e testada conforme perspectiva

--- src/main/java/com/apirestdemo/demo/controller/ProductController.java
+++ src/main/java/com/apirestdemo/demo/controller/ProductController.java
@@ -1,8 +1,28 @@
 package com.apirestdemo.demo.controller;
 
+import com.apirestdemo.demo.entities.Department;
+import com.apirestdemo.demo.entities.Products;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
+@RequestMapping(value = "/products")
 public class ProductController {
-    
+    @GetMapping
+    public List<Products> getList(){
+        Department d1 = new Department(1L, "Tech");
+        Department d2 = new Department(2L, "Pet");
+
+        Products p1 = new Products(1L, "Macbook Pro", 4000.0, d1);
+        Products p2 = new Products(2L, "PC Gamer", 5000.0, d1);
+        Products p3 = new Products(3L, "Pet House", 300.0, d2);
+
+        List<Products> list = Arrays.asList(p1,p2,p3);
+
+        return list;
+    }
 }
---
+++ src/main/java/com/apirestdemo/demo/entities/Products.java
@@ -0,0 +1,50 @@
+package com.apirestdemo.demo.entities;
+
+public class Products {
+    private Long id;
+    private String name;
+    private Double price;
+
+    private Department department;
+
+    public Products(){
+    }
+    public Products(Long id, String name, Double price, Department department) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.department = department;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+}
